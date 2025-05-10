a14274c69c58f0653300332bac3dbfb9a0b9ee2d
Mouncif25 <mouncif.benyezzou@etu.uae.ac.ma>
2025-05-10 21:53:42 +0100

    new changes

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-  examId: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
-  type: { type: String, enum: ['qcm', 'direct'], required: true },
+  examId: { type: String, required: true },
+  type: { type: String, enum: ["qcm", "direct"], required: true },
   text: { type: String, required: true },
   options: [String], // only for QCM
   answer: { type: String, required: true },
   points: { type: Number, required: true },
-  time: { type: Number, required: true } // in seconds
+  time: { type: Number, required: true }, // in seconds
 });
 
-module.exports = mongoose.model('Question', questionSchema);
+module.exports = mongoose.model("Question", questionSchema);
